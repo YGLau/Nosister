3999dd15d0223defe20f21315bf51342d29a4eee
Mac <liuyonggang@liuyonggangdemacbook-pro.local>
2016-02-24 22:02:40 +0800

    导航按钮的一些点击细节搞定

--- 百思不得姐/百思不得姐/Classes/Essence/Controller/YGEssenceViewController.m
+++ 百思不得姐/百思不得姐/Classes/Essence/Controller/YGEssenceViewController.m
@@ -15,7 +15,10 @@
  *  红色指示器View
  */
 @property (weak, nonatomic) UIView *indicatorView;
-
+/**
+ *  当前选中按钮
+ */
+@property (weak, nonatomic) UIButton *selectedBtn;
 @end
 
 @implementation YGEssenceViewController
@@ -35,11 +38,19 @@
 - (void)setupTitlesView
 {
     UIView *titlesView = [[UIView alloc] init];
-    titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
+    titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.7];
     titlesView.width = self.view.width;
     titlesView.height = 35;
     titlesView.y = 64;
     [self.view addSubview:titlesView];
+    
+    // 添加红色指示器View
+    UIView *indicatorView = [[UIView alloc] init];
+    indicatorView.backgroundColor = [UIColor redColor];
+    indicatorView.height = 2;
+    indicatorView.y  = titlesView.height - indicatorView.height;
+    [titlesView addSubview:indicatorView];
+    self.indicatorView = indicatorView;
     
     // 内部5个按钮
     NSArray *titles = @[@"全部", @"视频", @"声音", @"图片", @"段子"];
@@ -53,20 +64,23 @@
         
         [button setTitle:titles[i] forState:UIControlStateNormal];
         [button setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor redColor] forState:UIControlStateDisabled];
         button.titleLabel.font = [UIFont systemFontOfSize:14];
         
         // 给按钮添加点击事件
         [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
         [titlesView addSubview:button];
+        
+        if (i == 0) {
+            button.enabled = NO;
+            self.selectedBtn = button;
+            [button.titleLabel sizeToFit];
+            self.indicatorView.width = button.titleLabel.width;
+            self.indicatorView.centerX = button.centerX;
+        }
     }
     
-    // 添加红色指示器View
-    UIView *indicatorView = [[UIView alloc] init];
-    indicatorView.backgroundColor = [UIColor redColor];
-    indicatorView.height = 2;
-    indicatorView.y  = titlesView.height - indicatorView.height;
-    [titlesView addSubview:indicatorView];
-    self.indicatorView = indicatorView;
+    
 }
 
 /**
@@ -74,9 +88,13 @@
  */
 - (void)buttonClick:(UIButton *)button
 {
+    self.selectedBtn.enabled = YES;
+    button.enabled = NO;
+    self.selectedBtn = button;
+    
     [UIView animateWithDuration:0.25 animations:^{
-        self.indicatorView.centerX = button.centerX;
         self.indicatorView.width = button.titleLabel.width;
+        self.indicatorView.centerX = button.centerX;
     }];
     
 }
@@ -97,9 +115,5 @@
     [self.navigationController pushViewController:RT animated: YES];
 }
 
-//- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-//{
-//    YGTestViewController *vc = [[YGTestViewController alloc] init];
-//    [self.navigationController pushViewController:vc animated:YES];
-//}
+
 @end
