6b750cb5c97d260ed6b76bbf5919b5594c73e650
Mac <liuyonggang@liuyonggangdemacbook-pro.local>
2016-03-03 17:09:23 +0800

    header细节处理

--- 百思不得姐/百思不得姐/Classes/Essence/Controller/YGCommentViewController.m
+++ 百思不得姐/百思不得姐/Classes/Essence/Controller/YGCommentViewController.m
@@ -10,7 +10,7 @@
 #import "YGTopic.h"
 #import "YGTopicCell.h"
 
-@interface YGCommentViewController () <UITableViewDelegate>
+@interface YGCommentViewController () <UITableViewDelegate, UITableViewDataSource>
 /**
  *  底部工具条间距
  */
@@ -42,16 +42,27 @@
     
     // 监听键盘的弹出或隐藏
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillChangeFrame:) name:UIKeyboardWillChangeFrameNotification object:nil];
+    //tableView设置全局色
+    self.tableView.backgroundColor = YGGlobalBg;
 }
 /**
  *  设置头部cell
  */
 - (void)setupHeader
 {
+    UIView *header = [[UIView alloc] init];
+    
     YGTopicCell *cell = [YGTopicCell cell];
     cell.topic = self.topic;
-    cell.height = self.topic.cellHeight;
-    self.tableView.tableHeaderView = cell;
+    // Cell的高度
+    cell.size = CGSizeMake(YGmainScreenW, self.topic.cellHeight);
+    [header addSubview:cell];
+    
+    //header的高度
+    header.height = cell.height + YGTopicCellMargin;
+    
+    self.tableView.tableHeaderView = header;
+    
 }
 - (void)keyboardWillChangeFrame:(NSNotification *)note
 {
@@ -75,5 +86,28 @@
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
     [self.view endEditing:YES];
+}
+
+#pragma mark - UITableViewDataSource方法
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return @"评论";
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"comment"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"comment"];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%zd -- %zd", indexPath.section, indexPath.row];
+    return cell;
 }
 @end
